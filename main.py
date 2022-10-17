92f1d643c87e45e1caad5915912626f6e71de85e
chaLyoung <dreamlovinghigh57@gmail.com>
2022-10-17 18:55:51 +0900

    make mq info

--- main.py
+++ main.py
@@ -3,6 +3,27 @@ import pika
 
 class Main:
     def __init__(self):
+        self.url = 'ip'
+        self.port = 'port'
+        self.vhost = '/'
+        self.cred = pika.PlainCredentials('id', 'passwd')
+        self.queue = 'queue_name'
+        return
+
+    def on_message(ch, method, header, body):
+        print("received %s" %body)
+        return
+
+    def main(self):
+        connection = pika.BlockingConnection(pika.ConnectionParameters(self.url, self.port, self.vhost, self.cred))
+        channel = connection.channel()
+        channel.basic_consume(
+            queue=self.queue
+            , on_message_callback=Main.on_message
+            , auto_ack=True
+        )
+        print("Consumer is starting ... ")
+        channel.start_consuming()
         return
 
 
